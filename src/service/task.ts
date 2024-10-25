17d2bf3a256876b63aba26c81b003c7e674f9785
huypl53 <phamlehuy52@gmail.com>
2024-10-25 09:43:22 +0700

    update

--- src/service/task.ts
+++ src/service/task.ts
@@ -10,7 +10,15 @@ export const getImage = (fileId: string) => {
     responseType: 'arraybuffer',
   });
 };
-
+// a đang để type filter client chỉ có 2 loạn đơn giản là ocr và dịch thôi
+// type=1 là ocr sẽ lấy ra 3 giá trị type 1, 5, 6
+// filter=2 thì lấy về 2 3 4 6
+// ocr = 1
+// translate = 2
+// manualTranslate = 3
+// ocrTranslate = 4
+// manualOCR = 5
+// manualOCRTranslate = 6
 export const getTaskHistory = (skip = 0, take = 20, type?: ETaskType) => {
   return httpGet()<ITaskHistory[]>(
     `/tasks/history?skip=${skip}&take=${take}&type=${type}`
