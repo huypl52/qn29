cff94b8178a15f6cd4c0e8c8df4539bb0a7ac99d
huypl53 <phamlehuy52@gmail.com>
2024-11-29 15:57:00 +0700

    update translte

--- src/service/translate.ts
+++ src/service/translate.ts
@@ -1,11 +1,11 @@
+import { DLang } from '~/type';
+import { ITranslateOcr } from '~/type/ocr';
 import {
   ISavedTranslation,
   ITranslation,
   ITranslationResult,
 } from '~/type/translate';
 import { httpPost } from './_req';
-import { DLang } from '~/type';
-import { ITranslateOcr } from '~/type/ocr';
 
 const defaultTranslateConf: ITranslation = {
   source_language: DLang.zh,
@@ -15,7 +15,7 @@ const defaultTranslateConf: ITranslation = {
 };
 
 export const translate = ({ source_text }: ITranslation) => {
-  return httpPost(1)<ITranslationResult>('/translate', {
+  return httpPost(0)<ITranslationResult>('/proxy/translate?', {
     ...defaultTranslateConf,
     source_text,
   });
